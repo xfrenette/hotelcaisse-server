a00624e552430292934fe04ee3df808b1451eac9
Xavier Frenette <xavier@xavierfrenette.com>
2017-07-15 16:39:33 -0400

    Created ApiSession, Business and Device models + respective migrations

---
+++ app/ApiSession.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ApiSession extends Model
+{
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'expires_at',
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function business()
+    {
+        return $this->belongsTo('App\Business');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function device()
+    {
+        return $this->belongsTo('App\Device');
+    }
+}
---
+++ database/migrations/2017_07_15_193919_create_api_sessions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApiSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('api_sessions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->uuid('token')->unique();
+            $table->dateTime('expires_at');
+            $table->integer('business_id')->unsigned();
+            $table->integer('device_id')->unsigned();
+
+            $table->foreign('business_id')->references('id')->on('businesses');
+            $table->foreign('device_id')->references('id')->on('devices');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('api_sessions');
+    }
+}
