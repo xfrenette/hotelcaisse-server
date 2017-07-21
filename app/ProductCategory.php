ec90b4ef94ba18d8d5936e194065a6ff59654785
Xavier Frenette <xavier@xavierfrenette.com>
2017-07-21 12:15:07 -0400

    Created ProductCategory

---
+++ app/ProductCategory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductCategory extends Model
+{
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function business()
+    {
+        return $this->belongsTo('App\Business');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function parent()
+    {
+        return $this->belongsTo('App\ProductCategory', 'parent_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function categories()
+    {
+        return $this->hasMany('App\ProductCategory', 'parent_id');
+    }
+}
---
+++ database/migrations/2017_07_21_161216_create_product_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name', 100);
+            $table->integer('parent_id')->unsigned()->nullable();
+            $table->integer('business_id')->unsigned();
+
+            $table->foreign('parent_id')->references('id')->on('product_categories');
+            $table->foreign('business_id')->references('id')->on('businesses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_categories');
+    }
+}
