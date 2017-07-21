d75d5efe30b1474519db1517ed3661d60b0a5cda
Xavier Frenette <xavier@xavierfrenette.com>
2017-07-21 12:47:35 -0400

    Created product_tax relationship table, but not yet Product->taxes

---
+++ database/migrations/2017_07_21_164345_create_product_tax_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTaxTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_tax', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->float('amount', 8, 5);
+            $table->enum('type', ['percentage', 'absolute'])->default('percentage');
+            $table->integer('product_id')->unsigned();
+            $table->integer('tax_id')->unsigned();
+
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('tax_id')->references('id')->on('taxes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_tax');
+    }
+}
