3045d19ebf4524e210f856431f5de26e874871df
Xavier Frenette <xavier@xavierfrenette.com>
2017-07-21 12:18:48 -0400

    Created Field

---
+++ database/migrations/2017_07_21_161519_create_fields_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFieldsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fields', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('type', 50);
+            $table->string('label', 200);
+            $table->string('role', 100)->nullable();
+            $table->boolean('required')->default(false);
+            $table->string('defaultValue', 255)->nullable();
+            $table->string('values', 255)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fields');
+    }
+}
