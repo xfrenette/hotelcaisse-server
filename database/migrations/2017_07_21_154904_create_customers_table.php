efda95b8fc76cbd64d91c20024938a5459ab2ff2
Xavier Frenette <xavier@xavierfrenette.com>
2017-07-21 11:49:58 -0400

    Created Customer

---
+++ database/migrations/2017_07_21_154904_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('business_id')->unsigned();
+
+            $table->foreign('business_id')->references('id')->on('business');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
