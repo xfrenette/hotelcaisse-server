3814a8367504dc2ae1ed7eb14be27f4cee878d87
Xavier Frenette <xavier@xavierfrenette.com>
2017-09-23 20:46:27 -0400

    PreCalcRegisterCashMovements job implemented and tested

---
+++ tests/Unit/Jobs/PreCalcRegisterCashMovementsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature\Jobs;
+
+use App\Jobs\PreCalcRegisterCashMovements;
+use App\Register;
+use App\Repositories\PreCalcCache;
+use Mockery as m;
+use Tests\TestCase;
+
+class PreCalcRegisterCashMovementsTest extends TestCase
+{
+    protected function tearDown()
+    {
+        m::close();
+        parent::tearDown();
+    }
+
+    public function testHandle()
+    {
+        $totalCashMovements = -86.85;
+
+        $repo = m::mock(PreCalcCache::class);
+        $register = m::mock(Register::class)->makePartial();
+        $register->id = 4;
+        $register->shouldReceive('getCashMovementsTotalAttribute')->andReturn($totalCashMovements);
+
+        $repo->shouldReceive('set')->once()->with($register, Register::PRE_CALC_CASH_MV_TOTAL, $totalCashMovements);
+
+        $job = new PreCalcRegisterCashMovements($register);
+        $job->handle($repo);
+    }
+}
